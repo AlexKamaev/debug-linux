4f4bec7ac3a61dca6ef2ae781f7ded66f2372d66
Alex Kamaev <alexikam@gmail.com>
2019-11-07 12:54:40 +0300

    123

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,10 @@
 fixture `fixture`
     .page `http://example.com`;
 
-test('test', async t => {
+test('test 1', async t => {
+   await t.click('h1');
+});
+
+test('test 2', async t => {
    await t.click('h1');
 });
